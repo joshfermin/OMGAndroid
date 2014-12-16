063442d1898aa81ec4ce626da6b59de112371021
joshfermin <joshfermin@gmail.com>
2014-12-15 23:28:16 -0700

    finished up sendMessage

--- app/src/main/java/com/example/joshfermin/omgandroid/MainActivity.java
+++ app/src/main/java/com/example/joshfermin/omgandroid/MainActivity.java
@@ -6,8 +6,15 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.content.Intent;
 import android.view.View;
+import android.widget.EditText;
+
+
+
+
+
 
 public class MainActivity extends ActionBarActivity {
+    public final static String EXTRA_MESSAGE = "com.mycompany.myfirstapp.MESSAGE";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,6 +46,11 @@ public class MainActivity extends ActionBarActivity {
     }
 
     public void sendMessage(View view){
-        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        Intent intent = new Intent(this, DisplayMessageActivity.class); // this is a reference to the current object — the object whose method or constructor is being called.
+                                                                        // intent is passed to the class
+        EditText editText = (EditText) findViewById(R.id.edit_message); // findViewByID gets the EditText element
+        String message = editText.getText().toString(); // assign the editText (enter a message) to local var message
+        intent.putExtra(EXTRA_MESSAGE, message); // intent carries datatypes as key value pairs called extras (putExtra(key,value))
+        startActivity(intent); // starts intent
     }
 }
