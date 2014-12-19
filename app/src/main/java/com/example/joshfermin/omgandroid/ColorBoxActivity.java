961dfb113227825bf007bd1d7b897bb2bb594a68
joshfermin <joshfermin@gmail.com>
2014-12-19 01:41:38 -0700

    GOT THE COLORS TO WORK (BLUE/GREEN/RED). When you type blue or green or red it will return the color to you

--- app/src/main/java/com/example/joshfermin/omgandroid/ColorBoxActivity.java
+++ app/src/main/java/com/example/joshfermin/omgandroid/ColorBoxActivity.java
@@ -1,10 +1,14 @@
 package com.example.joshfermin.omgandroid;
 
+import android.graphics.Color;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.content.Intent;
+import android.view.Surface;
+import android.view.SurfaceView;
+import android.view.View;
 import android.widget.TextView;
 
 //Every Activity is invoked by an Intent, regardless of how the user navigated there.
@@ -22,12 +26,27 @@ public class ColorBoxActivity extends ActionBarActivity {
         String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
 
         // Create the text view
-        TextView textView = new TextView(this);
-        textView.setTextSize(5);
-        textView.setText(message);
+
 
         // Set the text view as the activity layout
-        setContentView(textView);
+//        setContentView(textView);
+
+//        Color mColor = new Color();
+//        mColor.red();
+//        mColor.green(greenvalue);
+//        mColor.blue(bluevalue);
+
+        setContentView(R.layout.colorbox);
+        View surface = findViewById(R.id.surface);
+        if (message.equals("red")) {
+            surface.setBackgroundColor(Color.RED);
+        } else if (message.equals("blue")) {
+            surface.setBackgroundColor(Color.BLUE);
+        } else if (message.equals("green")) {
+            surface.setBackgroundColor(Color.GREEN);
+        }
+
+
     }
 
     @Override
